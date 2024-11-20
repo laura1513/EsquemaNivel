4edbd5adaa8f8776a6645f843bf4ab20173d73d9
Laura Jorques <laurajorques2003@gmail.com>
2024-11-20 20:45:42 +0100

    Escena con la interacción de la roca (solo salen particulas)

---
+++ Assets/Scripts/Interaccion/MoverCaja.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MoverCaja : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem particulas;
+    public bool activa; //la pongo p�blica para verla desde el inspector
+    private bool _particulasOn;
+
+    private void Start()
+    {
+
+        activa = true;
+        _particulasOn = false;
+        particulas.Stop();
+    }
+    public void SwitchParticulas()
+    {
+        if (activa)
+        {
+
+            if (_particulasOn) { particulas.Stop(); _particulasOn = false; } else { particulas.Play(); _particulasOn = true; }
+        }
+
+    }
+    public void ActivarPalanca()
+    {
+        activa = true;
+    }
+    public void DesactivarPalanca()
+    {
+        activa = false;
+    }
+}
